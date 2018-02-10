c5e100d048aa7324c74c04c03618929e0ee7c44a
mike <mbarnett1@madisoncollege.edu>
2018-02-09 19:52:51 -0600

    beam program successfuly generates drawings and calculates reactions. still has 2 button problem and disappearing diagram problem

--- src/main/webapp/beam.js
+++ src/main/webapp/beam.js
@@ -1,171 +1,185 @@
+function createDiagram() {
 
-//WINDOW VARIABLES
-var width = 1600;
-var height = 300;
+    //WINDOW VARIABLES
+    var width = 1600;
+    var height = 300;
 
-var stage = new Konva.Stage({
-  container: 'container',
-  width: width,
-  height: height
-});
+    var stage = new Konva.Stage({
+        container: 'container',
+        width: width,
+        height: height
+    });
 
-var stage2 = new Konva.Stage({
-  container: 'container2',
-  width: width,
-  height: height
-});
+    var stage2 = new Konva.Stage({
+        container: 'container2',
+        width: width,
+        height: height
+    });
 
-var layer =  new Konva.Layer();
-var layer2 = new Konva.Layer();
-
-//DRAWING VARIABLES
-var offsetX = 75 ;
-var offsetY = 100;
-var beamThickness = 50;
-var rollerSupportLocation = 500 ;
-var pinSupportLocation = 200;
+    var layer = new Konva.Layer();
+    var layer2 = new Konva.Layer();
 
 
-//SHAPES
-var rect = new Konva.Rect({
-  x: offsetX,
-  y: offsetY,
-  width: 1000,
-  height: beamThickness,
-  fill: '#1a75ff',
-  stroke: 'black',
-  strokeWidth: 2
-});
+    var length           = document.getElementById("length").value;
+    var scaleFactor = 1000/length;
 
-var rect2 = new Konva.Rect({
-  x: offsetX,
-  y: offsetY,
-  width: 1000,
-  height: beamThickness,
-  fill: '#1a75ff',
-  stroke: 'black',
-  strokeWidth: 2
-});
-
-var circle = new Konva.Circle({
-  x: offsetX + rollerSupportLocation,
-  y: offsetY + beamThickness + 25,
-  radius: 25,
-  fill: '#3dce33',
-  stroke: 'black',
-  strokeWidth: 2
-});
-
-var triangle = new Konva.RegularPolygon({
-  x: offsetX + pinSupportLocation,
-  y: 183,
-  sides: 3,
-  radius: 33,
-  fill: '#3dce33',
-  stroke: 'black',
-  strokeWidth: 2
-});
+    var length           = document.getElementById("length").value          *scaleFactor;
+    var pinLocation      = document.getElementById("pinLocation").value     *scaleFactor;
+    var rollerLocation   = document.getElementById("rollerLocation").value  *scaleFactor;
+    var loadLocation     = document.getElementById("loadLocation").value    *scaleFactor;
+    var loadMagnitude    = document.getElementById("loadMagnitude").value               ;            //DO NOT SCALE
+    var angle            = document.getElementById("angle").value                       ;            //DO NOT SCALE
+    var angle180         = Number(angle) + 180;
 
 
-// ARRROWS
-var loadArrow = new Konva.Arrow({
-  x: offsetX + 500,
-  y: offsetY - 6,
-  points: [0,20, 0,100],
-  pointerLength: 15,
-  pointerWidth : 15,
-  fill: 'red',
-  stroke: 'red',
-  strokeWidth: 5,
-  offsetY: 100,
-  rotation: 0
-});
-
-var pinArrowY = new Konva.Arrow({
-  x: offsetX + pinSupportLocation,
-  y: offsetY + beamThickness + 6,
-  points: [0,20, 0,100],
-  pointerLength: 15,
-  pointerWidth : 15,
-  fill: '#3dce33',
-  stroke: '#3dce33',
-  strokeWidth: 5,
-  offsetY: 100,
-  rotation: 180
-});
-
-var pinArrowX = new Konva.Arrow({
-  x: offsetX - 5 + pinSupportLocation,
-  y: offsetY + beamThickness,
-  points: [0,10, 0,100],
-  pointerLength: 15,
-  pointerWidth : 15,
-  fill: '#3dce33',
-  stroke: '#3dce33',
-  strokeWidth: 5,
-  offsetY: 100,
-  rotation: -90
-});
-
-var rollerArrow = new Konva.Arrow({
-  x: offsetX + rollerSupportLocation,
-  y: offsetY + beamThickness + 6,
-  points: [0,20, 0,100],
-  pointerLength: 15,
-  pointerWidth : 15,
-  fill: '#3dce33',
-  stroke: '#3dce33',
-  strokeWidth: 5,
-  offsetY: 100,
-  rotation: 180
-});
+    //DRAWING VARIABLES
+    var offsetX = 100;
+    var offsetY = 100;
+    var beamThickness = 50;
 
 
-//TEXT
-var rollerText = new Konva.Text({
-  x: rollerSupportLocation + 85,
-  y: 190,
-  text: 'Ry',
-  fontSize: 30,
-  fontFamily: 'Calibri',
-  fill: '#3dce33'
-});
+    //SHAPES
+    var rect = new Konva.Rect({
+        x: offsetX,
+        y: offsetY,
+        width: 1000,
+        height: beamThickness,
+        fill: '#1a75ff',
+        stroke: 'black',
+        strokeWidth: 2
+    });
 
-var pinTextY = new Konva.Text({
-  x: pinSupportLocation + 20,
-  y: 160,
-  text: 'Px',
-  fontSize: 30,
-  fontFamily: 'Calibri',
-  fill: '#3dce33'
-});
+    var rect2 = new Konva.Rect({
+        x: offsetX,
+        y: offsetY,
+        width: 1000,
+        height: beamThickness,
+        fill: '#1a75ff',
+        stroke: 'black',
+        strokeWidth: 2
+    });
 
-var pinTextX = new Konva.Text({
-  x: pinSupportLocation + 85,
-  y: 190,
-  text: 'Py',
-  fontSize: 30,
-  fontFamily: 'Calibri',
-  fill: '#3dce33'
-});
+    var circle = new Konva.Circle({
+        x: offsetX + rollerLocation,
+        y: offsetY + beamThickness + 25,
+        radius: 25,
+        fill: '#3dce33',
+        stroke: 'black',
+        strokeWidth: 2
+    });
 
-
-// ADD SHAPES TO LAYERS
-layer.add(rect);
-layer.add(circle);
-layer.add(triangle);
-layer.add(loadArrow);
-
-layer2.add(rect2);
-layer2.add(pinArrowY);
-layer2.add(pinArrowX);
-layer2.add(rollerArrow);
-
-layer2.add(rollerText);
-layer2.add(pinTextX);
-layer2.add(pinTextY);
+    var triangle = new Konva.RegularPolygon({
+        x: offsetX + pinLocation,
+        y: 183,
+        sides: 3,
+        radius: 33,
+        fill: '#3dce33',
+        stroke: 'black',
+        strokeWidth: 2
+    });
 
 
-// ADD LAYERS TO STAGE
-stage.add(layer);
-stage2.add(layer2)
+    // ARRROWS
+    var loadArrow = new Konva.Arrow({
+        x: offsetX + loadLocation,
+        y: offsetY + beamThickness/2,
+        points: [0, 20, 0, 100],
+        pointerLength: 15,
+        pointerWidth: 15,
+        fill: 'red',
+        stroke: 'red',
+        strokeWidth: 5,
+        offsetY: 100,
+        rotation: angle180
+    });
+
+    var pinArrowY = new Konva.Arrow({
+        x: offsetX + pinLocation,
+        y: offsetY + beamThickness + 6,
+        points: [0, 20, 0, 100],
+        pointerLength: 15,
+        pointerWidth: 15,
+        fill: '#3dce33',
+        stroke: '#3dce33',
+        strokeWidth: 5,
+        offsetY: 100,
+        rotation: 180
+    });
+
+    var pinArrowX = new Konva.Arrow({
+        x: offsetX - 5 + pinLocation,
+        y: offsetY + beamThickness,
+        points: [0, 10, 0, 100],
+        pointerLength: 15,
+        pointerWidth: 15,
+        fill: '#3dce33',
+        stroke: '#3dce33',
+        strokeWidth: 5,
+        offsetY: 100,
+        rotation: -90
+    });
+
+    var rollerArrow = new Konva.Arrow({
+        x: offsetX + rollerLocation,
+        y: offsetY + beamThickness + 6,
+        points: [0, 20, 0, 100],
+        pointerLength: 15,
+        pointerWidth: 15,
+        fill: '#3dce33',
+        stroke: '#3dce33',
+        strokeWidth: 5,
+        offsetY: 100,
+        rotation: 180
+    });
+
+
+    //TEXT
+    var rollerText = new Konva.Text({
+        x: rollerLocation + 110,
+        y: 190,
+        text: 'Ry',
+        fontSize: 30,
+        fontFamily: 'Calibri',
+        fill: '#3dce33'
+    });
+
+    var pinTextY = new Konva.Text({
+        x: pinLocation + 30,
+        y: 160,
+        text: 'Px',
+        fontSize: 30,
+        fontFamily: 'Calibri',
+        fill: '#3dce33'
+    });
+
+    var pinTextX = new Konva.Text({
+        x: pinLocation + 105,
+        y: 185,
+        text: 'Py',
+        fontSize: 30,
+        fontFamily: 'Calibri',
+        fill: '#3dce33'
+    });
+
+
+    // ADD SHAPES TO LAYERS
+    layer.add(rect);
+    layer.add(circle);
+    layer.add(triangle);
+    layer.add(loadArrow);
+
+    layer2.add(rect2);
+    layer2.add(pinArrowY);
+    layer2.add(pinArrowX);
+    layer2.add(rollerArrow);
+
+    layer2.add(rollerText);
+    layer2.add(pinTextX);
+    layer2.add(pinTextY);
+
+
+    // ADD LAYERS TO STAGE
+    stage.add(layer);
+    stage2.add(layer2)
+
+}
--- src/main/webapp/beam.jsp
+++ src/main/webapp/beam.jsp
@@ -6,36 +6,43 @@
 <meta charset="utf-8">
 <title>Beam</title>
 <script src="konva.js"> </script>
+<script src="beam.js"></script>
 
 
 <div class="container-fluid">
     <form action="/beamServlet" method="GET">
         Length:
-        <input type="text" name="length" id="length" value="100"><br>
+        <input type="text" name="length" id="length"><br>
         Pin Support Location (m):
-        <input type="text" name="pinLocation" id="pinLocation" value="0"><br>
+        <input type="text" name="pinLocation" id="pinLocation"><br>
         Roller Support Location (m):
-        <input type="text" name="rollerLocation" id="rollerLocation" value="100"><br>
+        <input type="text" name="rollerLocation" id="rollerLocation"><br>
         Load Location (m):
-        <input type="text" name="loadLocation" id="loadLocation" value="50"><br>
+        <input type="text" name="loadLocation" id="loadLocation"><br>
         Load Magnitude (N):
-        <input type="text" name="loadMagnitude" id="loadMagnitude" value="100"><br>
+        <input type="text" name="loadMagnitude" id="loadMagnitude"><br>
         Load Orientation (deg) 0 is up clockwise is positive:
-        <input type="text" name="angle" id="angle" value="180"><br>
+        <input type="text" name="angle" id="angle"><br>
 
-        <input type="submit" value="Submit">
+
+
+        <input type="submit" value="Submit"">
+
+
     </form>
 
     <div id="container"></div>
     <div id="container2"></div>
-    <script src="beam.js"></script>
+
+    <button onclick="createDiagram()">Create Diagram</button>
+
 
 
 
     <h2> beam length: ${beam.length} </h2>
-    <h2> pin reaction x: ${pinReactionX} </h2>
-    <h2> pin reaction y: ${pinReactionY} </h2>
-    <h2> roller reaction y: ${rollerReactionY} </h2>
+    <h2> pin reaction x: ${beam.pinReactionX} </h2>
+    <h2> pin reaction y: ${beam.pinReactionY} </h2>
+    <h2> roller reaction y: ${beam.rollerReactionY} </h2>
 
 
 
